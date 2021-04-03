927a9d6bb43154146834adaa72ca2a0feb408f9b
natalie <natalie.itzhak@gmail.com>
2021-04-03 14:06:20 +0300

    C:/Program Files/Git/

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 import React from 'react';
-// import HomePage from './Components/HomePage';
-import axios from 'axios';
+import HomePage from './Components/HomePage';
+
 
 function App() {
-  const APP_ID="2a245b8d";
-const APP_KEY="1b5d7651d5126b0169a372c4c7c7da36";
-    const Url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    const getData= async ()=> {
-        const res = await axios.get(Url);
-        console.log(res);
-        console.log(res.data.hits);
-    }
+
 
   return (
     <div className="App">
-<h1 >Food Recipes App</h1>
-<input></input>
-<button onClick={getData}>Search</button>
+<HomePage/>
     </div>
   );
 }
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,21 +2,31 @@ import React from 'react';
 import axios from 'axios';
 
 function HomePage() {
-const APP_ID="2a245b8d";
-const APP_KEY="1b5d7651d5126b0169a372c4c7c7da36";
+    const APP_ID = "2a245b8d";
+    const APP_KEY = "1b5d7651d5126b0169a372c4c7c7da36";
     const Url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    const getData= async ()=> {
+    const getData = async () => {
         const res = await axios.get(Url);
         console.log(res);
+        console.log(res.data.hits);
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        getData();
+
+    }
 
     return (
 
-        <div >
-<h1 onClick={getData}>Food Search App</h1>
+        <div className="App">
+            <h1 >Food Recipes App</h1>
+            <form className='search-form' onSubmit={onSubmit}>
+                <input type='text' placeholder='Search Food' autoComplete='off'></input>
+                <button type='submit' value='search' >Search</button>
+            </form>
         </div>
-    )
+    );
 }
 
 export default HomePage
